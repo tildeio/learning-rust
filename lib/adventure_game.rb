8841dcdb624c003a07005186800ae48b38e58746
Liz Baillie and Rocky Neurock <engineering+lb+rn@tilde.io>
2016-06-09 16:56:15 -0700

    Add documentation about what has happened

--- lib/adventure_game.rb
+++ lib/adventure_game.rb
@@ -1,33 +1,58 @@
 module AdventureGame
+  # A Location is a simple object that has an `x` and
+  # `y` coordinate. Both are numbers.
   class Location < Struct.new(:x, :y)
   end
 
+  # A Room is a simple object that has a `location`,
+  # (which is a Location object) and a description
+  # (which is a string).
   class Room < Struct.new(:location, :description)
+    # The Room is constructed with an x and y, which
+    # are passed to Location.new, so they should be
+    # numbers.
     def self.new(x, y, description)
       super Location.new(x, y), description
     end
 
+    # to_s prints out the x and y coordinates and
+    # the description.
     def to_s
-      "#{@location.x}, #{@location.y}, #{@description}"
+      "#{location.x}, #{location.y}, #{description}"
     end
   end
 
+  # A Map is an object that has a `title` as a string,
+  # a Hash of rooms (where the keys are Locations),
+  # a max_x, which is the east-most room, and max_y,
+  # which is the north-most room.
   class Map < Struct.new(:title, :rooms, :max_x, :max_y)
+    # A Map is initialized with a title and room list,
+    # which is an array of rooms, and the room list is
+    # converted into the expected Hash and max_x/max_y
+    # values.
     def initialize(title, room_list)
+      # Construct an empty Hash and initialize max_x and max_y
       rooms = {}
       max_x = 0
       max_y = 0
 
+      # Iterate over the room_list
       room_list.each do |room|
+        # extract x and y from the room's location
         x = room.location.x
         y = room.location.y
 
+        # update max_x and max_y if necessary
         max_x = [max_x, x].max
         max_y = [max_y, y].max
 
+        # insert the room into the rooms Hash with its location
+        # as a key
         rooms[room.location] = room
       end
 
+      # construct a new Map with title, rooms, max_x and max_y
       super title, rooms, max_x, max_y
     end
 
@@ -35,11 +60,23 @@ module AdventureGame
       self.rooms[location]
     end
 
+    # valid_directions takes a Location contained in this
+    # Map and answers which directions a player can go.
+    # Specifically, a player is not allowed to move off
+    # the edge of the map.
     def valid_directions(location)
       {
+        # a player can go north if they are not already
+        # at the max_y
         north: location.y < self.max_y,
+        # a player can go south if they are not at the
+        # bottom edge
         south: location.y > 0,
+        # a player can go east if they are not already
+        # at the max_x
         east: location.x < self.max_x,
+        # a player can go west if they are not at the
+        # left edge
         west: location.x > 0
       }
     end
--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@
 
 use std::collections::HashMap;
 use std::cmp::max;
+use std::fmt;
 
 pub type StringLiteral = &'static str;
 
@@ -13,7 +14,7 @@ struct Player {
 }
 
 impl Player {
-    fn new(name: StringLiteral, x: usize, y: usize) -> Player {
+    fn new(name: StringLiteral, x: u64, y: u64) -> Player {
         Player {
             name: name,
             location: Location { x: x, y: y }
@@ -21,49 +22,54 @@ impl Player {
     }
 }
 
+// We want to be able to debug Location, `==` it (which requires
+// Eq and PartialEq), use it as a HashMap key, and clone it.
 #[derive(Debug, Eq, PartialEq, Hash, Clone)]
 pub struct Location {
-    x: usize,
-    y: usize,
-}
+    x: u64, // it has an x and y, both of which are unsigned numbers
+    y: u64, // that means they cannot be negative (and we'll get an
+}           // error if we try to underflow)
 
+// The implementation is separate from the struct definition, which
+// just describes the data structure. You can have as many `impl`
+// blocks as you want, and they get merged together.
 impl Location {
-    fn new(x: usize, y: usize) -> Location {
+    // This provides Location::new(), which takes two `u64`s and
+    // produces a `Location` object.
+    fn new(x: u64, y: u64) -> Location {
+        // Note that the lack of a `self` parameter is what makes
+        // this a "class method", which means it is invoked as
+        // Location::new(x, y).
         Location { x: x, y: y }
     }
 }
 
+// We want to be able to debug Room and `==` it.
 #[derive(Debug, Eq, PartialEq)]
 pub struct Room {
-    location: Location,
-    description: String,
+    location: Location,   // it has a Location
+    description: String,  // it has a description
 }
 
 impl Room {
-    fn new(x: usize, y: usize, description: StringLiteral) -> Room {
-        Room {
-            location: Location { x: x, y: y },
-            description: description.to_string(),
-        }
-    }
-
-    fn from_string(x: usize, y: usize, description: String) -> Room {
-        Room {
-            location: Location { x: x, y: y },
-            description: description,
+    // As in Ruby, we make Room::new take an x, y and description,
+    // and construct a Location using the x and y.
+    fn new(x: u64, y: u64, description: String) -> Room { // returns a Room
+        Room { // Construct a Room
+            location: Location::new(x, y),  // construct a Location
+            description: description,       // save the description
         }
     }
 }
 
+// We want to be able to debug Room and `==` it.
 #[derive(Debug, Eq, PartialEq)]
 pub struct Map {
     title: String,
     rooms: HashMap<Location, Room>,
-    max_x: usize,
-    max_y: usize
+    max_x: u64, // east-most room
+    max_y: u64  // north-most room
 }
-
-use std::fmt;
 
 #[derive(Debug, Eq, PartialEq)]
 pub struct ValidDirection {
@@ -74,22 +80,32 @@ pub struct ValidDirection {
 }
 
 impl Map {
-    fn new(title: StringLiteral, mut room_list: Vec<Room>) -> Map {
+    fn new(title: StringLiteral, room_list: Vec<Room>) -> Map {
+        // Make a new mutable HashMap
         let mut rooms = HashMap::new();
 
+        // Make new mutable max_x and max_y values, initialized to 0
         let mut max_x = 0;
         let mut max_y = 0;
 
-        for room in room_list.drain(..) {
+        // Iterate over the room_list
+        for room in room_list {
+            // extract x and y from the room's location
             let x = room.location.x;
             let y = room.location.y;
 
+            // update max_x and max_y if necessary
             max_x = max(max_x, x);
             max_y = max(max_y, y);
 
+            // insert the room into the rooms Hash with its location
+            // as a key. clone the location because removing the
+            // location from the room to use as a key prevents use
+            // from using the room as a value.
             rooms.insert(room.location.clone(), room);
         }
 
+        // construct a new Map
         Map {
             title: title.to_string(),
             rooms: rooms,
@@ -98,8 +114,13 @@ impl Map {
         }
     }
 
+    // valid_directions takes a Location contained in this
+    // Map and answers which directions a player can go.
+    // Specifically, a player is not allowed to move off
+    // the edge of the map.
     fn valid_directions(&self, l: &Location) -> ValidDirection {
         ValidDirection {
+            // identical to the Ruby code
             north: l.y < self.max_y,
             south: l.y > 0,
             east: l.x < self.max_x,
@@ -137,11 +158,17 @@ impl fmt::Display for Map {
     }
 }
 
-pub fn room(x: usize, y: usize, desc: StringLiteral) -> Room {
-    Room::new(x, y, desc)
+// A helper function for easily contructing a room to pass to Map::new.
+// Used below in tests.
+pub fn room(x: u64, y: u64, desc: StringLiteral) -> Room {
+    // In Rust, string literals are "slices", which means they are
+    // shared, but we want an owned String. We can use to_string()
+    // to copy the StringLiteral into something we can own.
+    Room::new(x, y, desc.to_string())
 }
 
 fn main() {
+    // vec![] is the Array literal syntax in Rust.
     let rooms = vec![
         room(0, 2, "top left"),    room(1, 2, "top center"),    room(2, 2, "top right"),
         room(0, 1, "middle left"), room(1, 1, "middle center"), room(2, 1, "middle right"),
@@ -155,10 +182,12 @@ fn main() {
     println!("{:?}", player);
 }
 
+// cfg(test) means only include this code when compiling for test mode
 #[cfg(test)]
-mod tests {
-    use super::*;
+mod tests {       // this is a nested module
+    use super::*; // include all the public items from the parent module 
 
+    // helper function for constructing a 3x3 list of rooms for testing
     fn rooms() -> Vec<Room> {
         vec![
             room(0, 2, "top left"),    room(1, 2, "top center"),    room(2, 2, "top right"),
@@ -167,10 +196,13 @@ mod tests {
         ]
     }
 
-    fn simple_room(x: usize, y: usize) -> Room {
-      Room::from_string(x, y, format!("{}, {}", x, y))
+    // helper function for constructing a room whose description is
+    // just its `x, y` coordinates.
+    fn simple_room(x: u64, y: u64) -> Room {
+      Room::new(x, y, format!("{}, {}", x, y))
     }
 
+    // helper function for constructing a 4x4 list of rooms.
     fn big_rooms() -> Vec<Room> {
       vec![
         simple_room(0, 3), simple_room(1, 3), simple_room(2, 3), simple_room(3, 3),
@@ -188,12 +220,14 @@ mod tests {
         Map::new("Liz's Great Adventure", big_rooms())
     }
 
-    #[test]
+    #[test] // tell the test runner that this is a test function
     fn valid_directions_for_bottom_left() {
         let map = map();
         let location = Location { x: 0, y: 0 };
         let valid_directions = map.valid_directions(&location);
 
+        // If the two sides are not equal, panic and fail the
+        // test. This assumes both sides are Eq and PartialEq.
         assert_eq!(valid_directions, ValidDirection {
             north: true,
             south: false,
@@ -205,13 +239,16 @@ mod tests {
     #[test]
     fn looks_up_rooms_by_coordinates() {
         let map = map();
+        // Indexing a HashMap does not take ownership of the value
+        // passed to `[]`, so we lend the Location we create here
+        // by using `&`.
         assert_eq!(map.rooms[&Location::new(0, 2)], rooms()[0]);
     }
 
     #[test]
     fn valid_directions_for_top_right() {
         let map = map();
-        let location = Location { x: 2, y: 2 };
+        let location = Location::new(2, 2);
         let valid_directions = map.valid_directions(&location);
 
         assert_eq!(valid_directions, ValidDirection {
@@ -225,7 +262,7 @@ mod tests {
     #[test]
     fn valid_directions_for_top_right_in_big_map() {
         let map = big_map();
-        let location = Location { x: 3, y: 3 };
+        let location = Location::new(3, 3);
         let valid_directions = map.valid_directions(&location);
 
         assert_eq!(valid_directions, ValidDirection {
