687aef522fe39246c5477bc35dbbd8ded244c163
Franklin Webber <franklin.webber@gmail.com>
2017-05-03 16:29:28 -0500

    Updates the Ruby specs to operate again
    The lib/main.rb was doing two things. Acting as the central switchboard
    to load all the files for the project and then executing the gem. To get
    the tests to work there needed to be the first thing but not the second
    otherwise the game would start and then you would have to quit it for the
    tests to execute. So the 'lib/adventure.rb' is born which loads all the
    ruby files and now 'lib/main.rb' will require that file and start the game.
    
    To test this initial arrangement I wrote a single test file for the
    Location Struct.
    
    I removed the previous test related to the map specification because it
    referenced a few constants that no longer exist. When creating the tests
    for the Room and such this file can be revisited in the history to see
    if those tests can be resurrected and rebuilt.

--- lib/main.rb
+++ lib/main.rb
@@ -1,10 +1,3 @@
-require_relative 'game'
-require_relative 'inventory_item'
-require_relative 'location'
-require_relative 'main'
-require_relative 'map'
-require_relative 'npc'
-require_relative 'player'
-require_relative 'room'
+require_relative 'adventure'
 
 Game.play
